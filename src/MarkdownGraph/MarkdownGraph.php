3ad1f17d145be9084e15f843af79386632bcbcbd
jun00rbiter <jun00rbiter@gmail.com>
2017-04-23 02:38:34 +0900

    ヘッダに連番を振る

--- src/MarkdownGraph/MarkdownGraph.php
+++ src/MarkdownGraph/MarkdownGraph.php
@@ -13,36 +13,43 @@ namespace jun00rbiter\MarkdownGraph;
 
 use Michelf\MarkdownExtra;
 
-class MarkdownGraph extends MarkdownExtra {
+class MarkdownGraph extends MarkdownExtra
+{
+    public $dot_store_directory = __DIR__ . '/../../tmp';
+    public $svg_store_directory = __DIR__ . '/../../tmp';
+    public $url_prefix = "/tmp";
 
-	public $dot_store_directory = __DIR__ . '/../../tmp';
-	public $svg_store_directory = __DIR__ . '/../../tmp';
-	public $url_prefix = "/tmp";
+    public $chapter_no = 0;
+    public $section_no = 0;
+    public $paragraph_no = 0;
+    public $figure_no = 0;
+    public $table_no = 0;
 
-	/**
-	 * コンストラクタ
-	 * @return void
-	 */
-	public function __construct() {
-		$this->block_gamut += array(
-			"doGraphvizBlocks"	=> 100,
-		);
-		parent::__construct();
-	}
+    /**
+     * コンストラクタ
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->block_gamut += array(
+            "doGraphvizBlocks"    => 100,
+        );
+        parent::__construct();
+    }
 
-	/**
-	 * グラフブロックを抽出してHTMLに変換する。
-	 *
-	 * コードブロックと同じように、`@@@`の行でグラフスクリプトをはさむ。
-	 * グラフスクリプトを graphviz で処理して、svg ファイルを作成。
-	 * svg ファイルを object タグで読み込む HTML を出力する。
-	 * @return string
-	 */
-	protected function doGraphvizBlocks($text) {
-	
-		$less_than_tab = $this->tab_width;
-		
-		$text = preg_replace_callback('{
+    /**
+     * グラフブロックを抽出してHTMLに変換する。
+     *
+     * コードブロックと同じように、`@@@`の行でグラフスクリプトをはさむ。
+     * グラフスクリプトを graphviz で処理して、svg ファイルを作成。
+     * svg ファイルを object タグで読み込む HTML を出力する。
+     * @return string
+     */
+    protected function doGraphvizBlocks($text)
+    {
+        $less_than_tab = $this->tab_width;
+
+        $text = preg_replace_callback('{
 				(?:\n|\A)
 				# 1: Opening marker
 				(
@@ -57,7 +64,7 @@ class MarkdownGraph extends MarkdownExtra {
 					' . $this->id_class_attr_catch_re . ' # 3: Extra attributes
 				)?
 				[ ]* \n # Whitespace and newline following marker.
-				
+
 				# 4: Content
 				(
 					(?>
@@ -65,44 +72,108 @@ class MarkdownGraph extends MarkdownExtra {
 						.*\n+
 					)+
 				)
-				
+
 				# Closing marker.
 				\1 [ ]* (?= \n )
 			}xm',
-			array($this, '_doGraphvizBlocks_callback'), $text);
+            array($this, '_doGraphvizBlocks_callback'), $text);
 
-		return $text;
-	}
+        return $text;
+    }
 
-	/**
-	 * グラフブロックを処理するコールバック関数
-	 * @param  array $matches
-	 * @return string
-	 */
-	protected function _doGraphvizBlocks_callback($matches) {
-		$classname =& $matches[2];
-		$attrs     =& $matches[3];
-		$codeblock = $matches[4];
+    /**
+     * グラフブロックを処理するコールバック関数
+     * @param  array $matches
+     * @return string
+     */
+    protected function _doGraphvizBlocks_callback($matches)
+    {
+        $classname =& $matches[2];
+        $attrs     =& $matches[3];
+        $codeblock = $matches[4];
 
         $out = [];
         $filebase = md5($codeblock);
-        file_put_contents($this->dot_store_directory."/{$filebase}.dot",$codeblock);
+        file_put_contents($this->dot_store_directory."/{$filebase}.dot", $codeblock);
         exec(
-			"dot -Tsvg -o " . $this->svg_store_directory . "/{$filebase}.svg "
-			. $this->dot_store_directory . "/{$filebase}.dot", $out);
+            "dot -Tsvg -o " . $this->svg_store_directory . "/{$filebase}.svg "
+            . $this->dot_store_directory . "/{$filebase}.dot", $out);
 
-		$classes = array();
-		if ($classname != "") {
-			if ($classname{0} == '.')
-				$classname = substr($classname, 1);
-			$classes[] = $this->code_class_prefix . $classname;
-		}
-		$attr_str = $this->doExtraAttributes('img', $attrs, null, $classes);
+        $classes = array();
+        if ($classname != "") {
+            if ($classname{0} == '.') {
+                $classname = substr($classname, 1);
+            }
+            $classes[] = $this->code_class_prefix . $classname;
+        }
+        $attr_str = $this->doExtraAttributes('img', $attrs, null, $classes);
 
-		// $codeblock  = "<img$attr_str src=\"graph/{$filebase}.svg\" />";
-		$codeblock =
-			"<object$attr_str type=\"image/svg+xml\" "
-			. "data=\"{$this->url_prefix}/{$filebase}.svg\"></object>";
-		return "\n\n".$this->hashBlock($codeblock)."\n\n";
-	}
+        // $codeblock  = "<img$attr_str src=\"graph/{$filebase}.svg\" />";
+        $codeblock =
+            "<figure>\n".
+            "  <object$attr_str type=\"image/svg+xml\" data=\"{$this->url_prefix}/{$filebase}.svg\"></object>\n".
+            "  <figcaption>図のグラフ</figcaption>".
+            "</figure>";
+        return "\n\n".$this->hashBlock($codeblock)."\n\n";
+    }
+
+    public function transform($text)
+    {
+        $text = parent::transform($text);
+
+        $text = preg_replace_callback(
+            '{
+                (?:
+                    (^\<h           # $1: header tag
+                        ([123])     # $2: header level
+                        (.*?)       # $3: attributes
+                    [ ]*\>)
+                    (.*)            # $4: title
+                    (\</h\2\>)      # $5: close tag
+                    [ ]*\n+
+                )|(?:
+                    (^[ ]*\<figconfig\>
+                        (.*?)
+                        \</figcaption\>
+                        [ ]*\n+
+                    )
+                )
+            }mx',
+            array($this, '_doHeaders_callback_seq'), $text);
+
+        return $text;
+    }
+
+    /**
+     * Callback for setext headers
+     * @param  array $matches
+     * @return string
+     */
+    protected function _doHeaders_callback_seq($matches)
+    {
+        var_dump($matches);
+        $level =& $matches[2];
+        $attr  =& $matches[3];
+        $title =& $matches[4];
+
+        $header_str = '';
+        if ($level==1) {
+            $this->chapter_no++;
+            $this->section_no=0;
+            $this->paragraph_no=0;
+            $header_str = "{$this->chapter_no}. ";
+        } elseif ($level==2) {
+            $this->section_no++;
+            $this->paragraph_no=0;
+            $header_str = "{$this->chapter_no}.{$this->section_no}. ";
+        } elseif ($level==3) {
+            $this->paragraph_no++;
+            $header_str = "{$this->chapter_no}.{$this->section_no}.{$this->paragraph_no}. ";
+        }
+
+        $defaultId = is_callable($this->header_id_func) ? call_user_func($this->header_id_func, $matches[2]) : null;
+
+        $block = "<h$level$attr>$header_str $title</h$level>";
+        return "\n$block\n\n";
+    }
 }
